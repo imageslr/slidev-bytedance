33c2f1cc799de27c3e388fb68c856a67566a3af8
elonz <zhukaihao.r@bytedance.com>
2023-07-09 23:45:54 +0800

    Feat: 页码
    Change-Id: Ib0bf5e68cd32c21c9d3c865a543bc1b6e459b539

--- theme/layouts/default.vue
+++ theme/layouts/default.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="slidev-layout default">
-    <img src="/images/logo-2.png" class="logo"/>
+    <img src="/images/logo-2.png" class="logo" />
     <slot />
+    <div class="slide-number">
+      <SlideCurrentNo /> /
+      <SlidesTotal />
+    </div>
   </div>
 </template>
--- theme/layouts/two-cols.vue
+++ theme/layouts/two-cols.vue
@@ -32,12 +32,17 @@ const props = defineProps({
 
 <template>
   <div class="slidev-layout two-columns w-full h-full grid grid-cols-2" :class="props.layoutClass">
-    <img src="/images/logo-2.png" class="logo"/>
+    <img src="/images/logo-2.png" class="logo" />
     <div class="col-left" :class="props.class">
       <slot />
     </div>
     <div class="col-right" :class="props.class">
       <slot name="right" />
+    </div>
+
+    <div class="slide-number">
+      <SlideCurrentNo /> /
+      <SlidesTotal />
     </div>
   </div>
 </template>
